916e7333b798291b2a6422630f454d2755680823
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-23 11:45:33 -0400

    completed assignment

--- main.js
+++ main.js
@@ -9,3 +9,44 @@ for(let i = 0; i < dinoCross.length ; i++) {
         console.log(e.target)
     })
 }
+
+//make it fade
+let dinoFade = document.querySelectorAll('.feature #dino-fade li')
+// console.log(dinoFade)
+for(let i = 0; i < dinoFade.length ; i++) {
+    // console.log(dinoFade[i])
+    dinoFade[i].addEventListener('click', (e)=> {
+        dinoFade[i].style.opacity ="0"
+        console.log(e.target)
+    })
+}
+
+//make image collapse
+let dinoRow = document.querySelectorAll('#row img')
+console.log(dinoRow)
+const collapseRow = () => {
+for(let i = 0; i < dinoRow.length ; i++) {
+    // console.log(dinoRow[i])
+    dinoRow[i].addEventListener('click', (e)=> {
+        console.log(e.target)
+        //decrease image width
+        dinoRow[i].style.width ="0px"
+        
+    })
+}
+}
+collapseRow()
+//blow it all up
+let meteor = document.querySelector('#burn button')
+// console.log(meteor)
+meteor.addEventListener('click', (e)=> {
+    for(img of dinoRow) {
+        img.style.width ="0px"
+    }
+    for(pic of dinoFade) {
+        pic.style.opacity="0"
+    }
+    for(pic of dinoCross) {
+        pic.style.textDecoration="line-through"
+    }
+})
